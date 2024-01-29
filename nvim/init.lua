ba7f24f843b531af0c5d3936a738d29217d48b7b
Kevin VAILLANT <kevin.vaillant@amadeus.com>
2024-01-29 14:39:05 +0100

    Alacritty toml migration + nvim sync all files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,14 @@
 -- Plugins
-require "plugins"
-require "comment_conf"
-require "telescope_conf"
-require "lualine_conf"
-require "shade_conf"
-require "colorsheme_conf"
-require "treesitter_conf"
-require "cpm_conf"
-require "tree_conf"
+require("plugins")
+require("comment_conf")
+require("telescope_conf")
+require("lualine_conf")
+require("shade_conf")
+require("colorsheme_conf")
+require("treesitter_conf")
+require("cpm_conf")
+require("tree_conf")
 
 -- Config
-require "options"
-require "keymaps"
+require("options")
+require("keymaps")
